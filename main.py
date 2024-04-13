3a3e22a7d77f61b0f3d83f27eb5ad61438dd9ce5
yefer8 <75265472@continental.edu.pe>
2024-04-12 20:26:36 -0500

    Calcula el MCD

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-class CalculadoraMCD:
+class CalculadoraMCM:
     def __init__(self, a, b):
         """
-        Inicializa la calculadora con los dos números enteros para los cuales se calculará el MCD.
+        Inicializa la calculadora con los dos números enteros para los cuales se calculará el MCM.
         """
         self.a = a
         self.b = b
@@ -17,12 +17,20 @@ class CalculadoraMCD:
             a, b = b, a % b
         return a
 
+    def calcular_mcm(self):
+        """
+        Calcula el mínimo común múltiplo (MCM) de los dos números utilizando la relación entre el MCD y el MCM.
+        :return: El mínimo común múltiplo de self.a y self.b.
+        """
+        mcd = self.calcular_mcd()
+        return (self.a * self.b) // mcd
+
 # Pedir al usuario que ingrese dos números enteros
 num1 = int(input("Ingresa el primer número entero: "))
 num2 = int(input("Ingresa el segundo número entero: "))
 
-# Crear una instancia de la clase CalculadoraMCD
-calculadora = CalculadoraMCD(num1, num2)
+# Crear una instancia de la clase CalculadoraMCM
+calculadora = CalculadoraMCM(num1, num2)
 
-# Calcular y mostrar el MCD de los dos números
-print("El máximo común divisor de", num1, "y", num2, "es:", calculadora.calcular_mcd())
+# Calcular y mostrar el MCM de los dos números
+print("El mínimo común múltiplo de", num1, "y", num2, "es:", calculadora.calcular_mcm())
